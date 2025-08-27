7cbe9c2ad21e4346cb996638b1fcb10b918756fe
Abdul Rafay <139442889+rafayshahid22@users.noreply.github.com>
2025-08-27 18:45:19 +0500

    Create streamlit_app.py
    Copied from custom smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Title
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
+
+# Input field for name
+name_on_order = st.text_input("Name on Smoothie", "")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+# Get Snowflake session
+session = get_active_session()
+
+# Query fruit options table
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+
+# Multi-select widget directly from dataframe
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients:",
+    my_dataframe
+    , max_selections=5
+)
+
+# If user selected ingredients
+if ingredients_list:
+    ingredients_string = " ".join(ingredients_list)
+
+    # Submit button
+    time_to_insert = st.button('Submit order')
+
+    # if time_to_insert:
+    #     # Insert query with both name and ingredients
+    #     my_insert_stmt = f"""
+    #         INSERT INTO smoothies.public.orders (name_on_order, ingredients)
+    #         VALUES ('{name_on_order}', '{ingredients_string.strip()}')
+    #     """
+    #     st.write("Running query:", my_insert_stmt)  # Debugging ke liye
+    #     session.sql(my_insert_stmt).collect()
+
+
+    if time_to_insert:
+        # Insert query with both name and ingredients
+        my_insert_stmt = f"""
+            INSERT INTO smoothies.public.orders (name_on_order, ingredients)
+            VALUES ('{name_on_order}', '{ingredients_string.strip()}')
+        """
+        session.sql(my_insert_stmt).collect()
+
+        # Success message with customer name
+        st.success(f"✅ Your Smoothie is ordered, {name_on_order}! 🍹", icon="🥤")
